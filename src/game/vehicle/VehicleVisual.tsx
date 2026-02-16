4b9ebaf2471eaf5da74705dd04932c7c7fa091fe
Noah Lin <nglin34@gmail.com>
2026-02-16 16:27:58 -0500

    feat: improve car paint and material fidelity with clearcoat detailing

--- src/game/vehicle/VehicleVisual.tsx
+++ src/game/vehicle/VehicleVisual.tsx
@@ -29,22 +29,22 @@ export function VehicleVisual({ chassisRef, frontLeftSteerRef, frontRightSteerRe
     <group ref={chassisRef} position={[0, 0.55, 0]}>
       <mesh castShadow receiveShadow>
         <boxGeometry args={[1.78, 0.43, 3.52]} />
-        <meshStandardMaterial color="#bd3340" roughness={0.34} metalness={0.28} />
+        <meshPhysicalMaterial color="#be2f3f" roughness={0.28} metalness={0.35} clearcoat={0.82} clearcoatRoughness={0.16} />
       </mesh>
 
       <mesh position={[0, 0.26, -0.23]} castShadow receiveShadow>
         <boxGeometry args={[1.58, 0.28, 2.72]} />
-        <meshStandardMaterial color="#ca3e45" roughness={0.32} metalness={0.24} />
+        <meshPhysicalMaterial color="#cd3d47" roughness={0.24} metalness={0.32} clearcoat={0.78} clearcoatRoughness={0.14} />
       </mesh>
 
       <mesh position={[0, 0.54, -0.32]} castShadow>
         <boxGeometry args={[1.2, 0.24, 1.44]} />
-        <meshStandardMaterial color="#f7f6f2" roughness={0.18} metalness={0.1} />
+        <meshPhysicalMaterial color="#f8f5ee" roughness={0.16} metalness={0.08} clearcoat={0.38} clearcoatRoughness={0.26} />
       </mesh>
 
       <mesh position={[0, 0.61, -0.32]}>
         <boxGeometry args={[1.02, 0.2, 1.22]} />
-        <meshStandardMaterial color="#94b1c3" roughness={0.08} metalness={0.78} />
+        <meshPhysicalMaterial color="#90a7bb" roughness={0.08} metalness={0.78} clearcoat={0.58} clearcoatRoughness={0.08} />
       </mesh>
 
       <mesh position={[0, -0.08, -1.77]} castShadow receiveShadow>
@@ -99,11 +99,33 @@ export function VehicleVisual({ chassisRef, frontLeftSteerRef, frontRightSteerRe
 
       <mesh position={[0, 0.34, -0.97]}>
         <boxGeometry args={[1.12, 0.04, 0.72]} />
-        <meshStandardMaterial color="#9cb6c8" roughness={0.1} metalness={0.72} transparent opacity={0.78} />
+        <meshPhysicalMaterial color="#9cb6c8" roughness={0.08} metalness={0.72} transparent opacity={0.82} transmission={0.18} />
       </mesh>
       <mesh position={[0, 0.34, 0.43]}>
         <boxGeometry args={[1.02, 0.04, 0.52]} />
-        <meshStandardMaterial color="#7f98ad" roughness={0.12} metalness={0.66} transparent opacity={0.65} />
+        <meshPhysicalMaterial color="#7f98ad" roughness={0.1} metalness={0.66} transparent opacity={0.7} transmission={0.14} />
+      </mesh>
+
+      <mesh position={[0, 0.52, -1.16]} castShadow>
+        <boxGeometry args={[0.68, 0.07, 0.34]} />
+        <meshStandardMaterial color="#191b1f" roughness={0.48} metalness={0.24} />
+      </mesh>
+      <mesh position={[0, 0.52, -1.48]} castShadow>
+        <boxGeometry args={[0.46, 0.05, 0.28]} />
+        <meshStandardMaterial color="#24262d" roughness={0.52} metalness={0.2} />
+      </mesh>
+
+      <mesh position={[0, 0.78, -0.62]} castShadow receiveShadow>
+        <boxGeometry args={[1.24, 0.06, 0.18]} />
+        <meshStandardMaterial color="#2f333b" roughness={0.36} metalness={0.48} />
+      </mesh>
+      <mesh position={[-0.5, 0.84, -0.62]} castShadow receiveShadow>
+        <boxGeometry args={[0.08, 0.12, 0.16]} />
+        <meshStandardMaterial color="#242933" roughness={0.38} metalness={0.5} />
+      </mesh>
+      <mesh position={[0.5, 0.84, -0.62]} castShadow receiveShadow>
+        <boxGeometry args={[0.08, 0.12, 0.16]} />
+        <meshStandardMaterial color="#242933" roughness={0.38} metalness={0.5} />
       </mesh>
 
       <group position={[-0.86, -0.17, 1.08]}>
@@ -113,7 +135,7 @@ export function VehicleVisual({ chassisRef, frontLeftSteerRef, frontRightSteerRe
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]} castShadow>
           <cylinderGeometry args={[0.2, 0.2, 0.14, 22]} />
-          <meshStandardMaterial color="#d34f57" roughness={0.28} metalness={0.68} />
+          <meshPhysicalMaterial color="#db535d" roughness={0.24} metalness={0.74} clearcoat={0.62} clearcoatRoughness={0.14} />
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]}>
           <cylinderGeometry args={[0.1, 0.1, 0.15, 14]} />
@@ -128,7 +150,7 @@ export function VehicleVisual({ chassisRef, frontLeftSteerRef, frontRightSteerRe
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]} castShadow>
           <cylinderGeometry args={[0.2, 0.2, 0.14, 22]} />
-          <meshStandardMaterial color="#d34f57" roughness={0.28} metalness={0.68} />
+          <meshPhysicalMaterial color="#db535d" roughness={0.24} metalness={0.74} clearcoat={0.62} clearcoatRoughness={0.14} />
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]}>
           <cylinderGeometry args={[0.1, 0.1, 0.15, 14]} />
@@ -143,7 +165,7 @@ export function VehicleVisual({ chassisRef, frontLeftSteerRef, frontRightSteerRe
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]} castShadow>
           <cylinderGeometry args={[0.2, 0.2, 0.14, 22]} />
-          <meshStandardMaterial color="#d34f57" roughness={0.28} metalness={0.68} />
+          <meshPhysicalMaterial color="#db535d" roughness={0.24} metalness={0.74} clearcoat={0.62} clearcoatRoughness={0.14} />
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]}>
           <cylinderGeometry args={[0.1, 0.1, 0.15, 14]} />
@@ -158,7 +180,7 @@ export function VehicleVisual({ chassisRef, frontLeftSteerRef, frontRightSteerRe
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]} castShadow>
           <cylinderGeometry args={[0.2, 0.2, 0.14, 22]} />
-          <meshStandardMaterial color="#d34f57" roughness={0.28} metalness={0.68} />
+          <meshPhysicalMaterial color="#db535d" roughness={0.24} metalness={0.74} clearcoat={0.62} clearcoatRoughness={0.14} />
         </mesh>
         <mesh rotation={[0, 0, Math.PI / 2]}>
           <cylinderGeometry args={[0.1, 0.1, 0.15, 14]} />
